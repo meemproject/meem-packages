6249771d0225f6a606cb6a4a6eeaf96155a6119a
Ken Goldfarb <hello@kengoldfarb.com>
2022-04-27 18:24:48 -0400

    cleanup

--- packages/meem-contracts/scripts/createABI.ts
+++ packages/meem-contracts/scripts/createABI.ts
@@ -1,29 +1,29 @@
 import path from 'path'
 import fs from 'fs-extra'
-import accessControlABI from '../abi/contracts/Meem/facets/AccessControlFacet.sol/AccessControlFacet.json'
-import clippingABI from '../abi/contracts/Meem/facets/ClippingFacet.sol/ClippingFacet.json'
-import erc721ABI from '../abi/contracts/Meem/facets/ERC721Facet.sol/ERC721Facet.json'
-import meemAdminABI from '../abi/contracts/Meem/facets/MeemAdminFacet.sol/MeemAdminFacet.json'
-import meemBaseABI from '../abi/contracts/Meem/facets/MeemBaseFacet.sol/MeemBaseFacet.json'
-import meemPermissionsABI from '../abi/contracts/Meem/facets/MeemPermissionsFacet.sol/MeemPermissionsFacet.json'
-import meemQueryABI from '../abi/contracts/Meem/facets/MeemQueryFacet.sol/MeemQueryFacet.json'
-import meemSplitsABI from '../abi/contracts/Meem/facets/MeemSplitsFacet.sol/MeemSplitsFacet.json'
-import reactionsABI from '../abi/contracts/Meem/facets/ReactionFacet.sol/ReactionFacet.json'
-import initFacet from '../abi/contracts/Meem/InitDiamond.sol/InitDiamond.json'
-import meemDiamondABI from '../abi/contracts/MeemDiamond.sol/MeemDiamond.json'
+import accessControlABI from '../artifacts/contracts/Meem/facets/AccessControlFacet.sol/AccessControlFacet.json'
+import clippingABI from '../artifacts/contracts/Meem/facets/ClippingFacet.sol/ClippingFacet.json'
+import erc721ABI from '../artifacts/contracts/Meem/facets/ERC721Facet.sol/ERC721Facet.json'
+import meemAdminABI from '../artifacts/contracts/Meem/facets/MeemAdminFacet.sol/MeemAdminFacet.json'
+import meemBaseABI from '../artifacts/contracts/Meem/facets/MeemBaseFacet.sol/MeemBaseFacet.json'
+import meemPermissionsABI from '../artifacts/contracts/Meem/facets/MeemPermissionsFacet.sol/MeemPermissionsFacet.json'
+import meemQueryABI from '../artifacts/contracts/Meem/facets/MeemQueryFacet.sol/MeemQueryFacet.json'
+import meemSplitsABI from '../artifacts/contracts/Meem/facets/MeemSplitsFacet.sol/MeemSplitsFacet.json'
+import reactionsABI from '../artifacts/contracts/Meem/facets/ReactionFacet.sol/ReactionFacet.json'
+import initFacet from '../artifacts/contracts/Meem/InitDiamond.sol/InitDiamond.json'
+import meemDiamondABI from '../artifacts/contracts/MeemDiamond.sol/MeemDiamond.json'
 
 const combinedABI = [
-	...accessControlABI,
-	...clippingABI,
-	...erc721ABI,
-	...meemAdminABI,
-	...meemBaseABI,
-	...meemPermissionsABI,
-	...meemQueryABI,
-	...meemSplitsABI,
-	...meemDiamondABI,
-	...reactionsABI,
-	...initFacet
+	...accessControlABI.abi,
+	...clippingABI.abi,
+	...erc721ABI.abi,
+	...meemAdminABI.abi,
+	...meemBaseABI.abi,
+	...meemPermissionsABI.abi,
+	...meemQueryABI.abi,
+	...meemSplitsABI.abi,
+	...meemDiamondABI.abi,
+	...reactionsABI.abi,
+	...initFacet.abi
 ]
 
 const basePath = path.join(process.cwd(), 'types')
