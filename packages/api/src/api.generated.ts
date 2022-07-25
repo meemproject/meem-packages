4a785a3bfa288f39786f89034292ebfe7fe976fd
Ken Goldfarb <hello@kengoldfarb.com>
2022-07-25 16:06:33 -0400

    types

--- packages/api/src/api.generated.ts
+++ packages/api/src/api.generated.ts
@@ -259,16 +259,10 @@ export interface IMeemMetadataProperties {
 	parent_token_metadata?: Record<string, any> | null
 }
 
-export type IMeemContractType =
-	| 'meem'
-	| 'meem-club'
-	| 'meem-post'
-	| 'meem-publication'
-
 export interface IMeemContractAssociation {
-	meem_contract_type: IMeemContractType
+	meem_contract_type: string
 	address: string
-	tokenIds: string[]
+	tokenIds?: string[]
 }
 export interface IMeemMetadata {
 	name: string
@@ -281,23 +275,15 @@ export interface IMeemMetadata {
 	extension_properties?: Record<string, any>
 	associations?: IMeemContractAssociation[]
 }
-export interface IMeemContractMetadata {
-	meem_contract_type: IMeemContractType
-	version: string
-	spec: string
-	name: string
-	description: string
-	image: string
-	associations?: IMeemContractAssociation[]
-}
 
-export interface IMeemContractMetadata {
-	meem_contract_type: IMeemContractType
-	version: string
-	spec: string
-	name: string
-	description: string
-	image: string
+export interface IMeemMetadataLike {
+	meem_metadata_version: string
+	[key: string]: any
+}
+export interface IMeemContractMetadataLike {
+	meem_contract_type: string
+	meem_metadata_version: string
+	[key: string]: any
 }
 
 export enum OpenSeaDisplayType {
@@ -449,7 +435,7 @@ export interface IMeemContractBaseProperties {
 }
 
 export interface IMeemContractInitParams {
-	symbol: string
+	symbol?: string
 	name: string
 	contractURI: string
 	admins: string[]
@@ -786,16 +772,40 @@ export namespace CreateMeemContract {
 
 	export interface IRequestBody {
 		/** Contract metadata */
-		metadata: IMeemContractMetadata
+		metadata: IMeemContractMetadataLike
 
-		/** Contract base properties */
-		contractParams: IMeemContractInitParams
+		/** The symbol for the token. If omitted, will use a slug of the name */
+		symbol?: string
+
+		/** The name of the token */
+		name: string
+
+		/** Contract admins */
+		admins?: string[]
+
+		/** Special minter permissions */
+		minters?: string[]
+
+		/** The max number of tokens */
+		maxSupply: string
+
+		/** Whether the max supply is locked */
+		isMaxSupplyLocked?: boolean
+
+		/** Minting permissions */
+		mintPermissions?: IMeemPermission[]
+
+		/** Splits for minting / transfers */
+		splits?: IMeemSplit[]
+
+		/** Whether tokens can be transferred */
+		isTransferLocked?: boolean
 
 		/** If true, will mint a token to the admin wallet addresses  */
 		shouldMintAdminTokens?: boolean
 
-		/** The owner of the contract can upgrade */
-		contractOwnerAddress?: string
+		/** Admin token metadata */
+		adminTokenMetadata?: IMeemMetadataLike
 	}
 
 	export interface IResponseBody extends IApiResponseBody {
@@ -1716,8 +1726,8 @@ export namespace MintOriginalMeem {
 		/** The address of the Meem contract to mint token */
 		meemContractAddress: string
 
-		/** JSON (or stringified) metadata object to be used for the minted Meem */
-		metadata?: string | any
+		/** Metadata object to be used for the minted Meem */
+		metadata?: IMeemMetadataLike
 
 		/** The address where the Meem will be minted to. */
 		to: string
