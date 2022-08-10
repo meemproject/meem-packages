b8b9ba37f2735a10014bceec2667053e9ae7121b
Ken Goldfarb <hello@kengoldfarb.com>
2022-08-10 17:29:01 -0400

    latest

--- packages/meem-contracts/contracts/facets/Splits/RoyaltiesV2.sol
+++ packages/meem-contracts/contracts/facets/Splits/RoyaltiesV2.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.13;
 pragma abicoder v2;
 
-import {Part} from './SplitsFacet.sol';
+import {Part} from './LibSplits.sol';
 
 interface RoyaltiesV2 {
 	// event RoyaltiesSet(uint256 tokenId, LibPart.Part[] royalties);
