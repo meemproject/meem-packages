2553a4c1ad2fcdde5caded907f7f45b5175f49c3
Ken Goldfarb <hello@kengoldfarb.com>
2022-09-20 17:13:00 -0400

    dry

--- packages/react/src/contexts/walletContext.tsx
+++ packages/react/src/contexts/walletContext.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable import/named */
-import { ERC20, MeemAPI } from '@meemproject/api'
+import { ERC20, MeemAPI, chains } from '@meemproject/api'
 import WalletConnectProvider from '@walletconnect/web3-provider'
 import { providers, ethers } from 'ethers'
 import Cookies from 'js-cookie'
@@ -16,7 +16,6 @@ import React, {
 } from 'react'
 import useSWR from 'swr'
 import Web3Modal from 'web3modal'
-import { chains } from '../lib/chains'
 import { makeFetcher } from '../lib/fetcher'
 import log from '../lib/log'
 
