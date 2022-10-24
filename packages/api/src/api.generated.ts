ff36c3a5866109f404c46ff691880a70cbd9f1a3
Brandon Minch <brandonminch@gmail.com>
2022-10-24 10:22:24 -0600

    update api types

--- packages/api/src/api.generated.ts
+++ packages/api/src/api.generated.ts
@@ -656,6 +656,14 @@ export namespace MeemAPI {
 			description: string
 			imageUrl: string | null
 			members: string[]
+			rolePlatforms: {
+				guildPlatform: {
+					invite: string
+					platform: {
+						name: 'DISCORD'
+					}
+				}
+			}[]
 		}
 		memberMeemIds: IMeemIdentity[]
 	}
